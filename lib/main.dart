c3db6c2e405382142753d7233963733cc7ab11e7
adityamuhammadputra <adityamuhammadputra@gmail.com>
2019-07-11 00:19:38 +0700

    belajar fluuter pertama membuat appbar dan button

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Adityamuhammadputra",
+    home: new HalamanSatu(),
+  ));
+}
+
+class HalamanSatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: new AppBar(
+        backgroundColor: Colors.red[1000],
+        leading: new Icon(Icons.home),
+        title: new Center(
+          child: new Text('adityamuhammadputra'),
+        ), 
+        actions: <Widget>[
+          new Icon(Icons.sentiment_dissatisfied )
+        ],
+      ),
+      body: Center(
+        child: new Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const RaisedButton(
+            onPressed: null,
+            child: Text(
+              'Disabled',
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        ],
+      ),
+    ),
+    );
+  }
+}
+// void main() {
+//   runApp(new MaterialApp(
+//     home: new HalHallo(),
+//   ));
+// }
+
+ 
+// class HalHallo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       body: Center(
+//         child : new Text('hello akhirat yuhuuuu'),
+//       )
+//     );
+//   }
+// }
